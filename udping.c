b5a975d31d8780a3199164f5b83ca64604927594
Bryan Molin <bjmolin@g.clemson.edu>
2024-04-15 21:17:08 -0400

    Worked on the output requirements. -S flag impemented along with stderr right-justified alignment

--- udping.c
+++ udping.c
@@ -1,13 +1,16 @@
 /*
-* This is a functional getopt that already has default values estabilshed. We could move this into a helper function if you'd prefer, 
-* although I'm not sure how we would implement calling methods that way. 
+*	James Williams
+*   Bryan Molina Cervantes
+*	Assignment 3
+*
 */
 
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdbool.h> // Include bool type
+#include <stdbool.h> 
+// #include "helper.c"
 
 int main(int argc, char **argv) {
     char cflag[30] = "0x7fffffff";
@@ -15,30 +18,41 @@ int main(int argc, char **argv) {
     int pflag = 33333;
     int sflag = 12;
     bool nflag = false; 
+    bool serverflag = false;
     char *cvalue = NULL;
     int index;
     int c;
-
+    
     opterr = 0;
-    // c:i:p:s:n contains the flags we will use but I don't recall what the colons mean (they're supposedly used to specify required flags)
-    while ((c = getopt(argc, argv, "c:i:p:s:n")) != -1) { 
+    // c:i:p:s:n contains the flags we will use but I don't recall 
+    // what the colons mean (they're supposedly used to specify required flags)
+    // Additionally, everything is sent to the stderr as specified in the doc
+    while ((c = getopt(argc, argv, "c:i:p:s:nS")) != -1) { 
         switch (c) {
-            case 'c':
-                snprintf(cflag, sizeof(cflag), "%s", optarg); 
+            case 'c': // ping-packet-count
+                snprintf(cflag, sizeof(cflag), "%s", optarg);
+                fprintf(stderr, "Count\t%10s\n", cflag);
                 break;
-            case 'i':
-                iflag = atof(optarg); // Convert optarg to float
+            case 'i': // ping-interval
+                iflag = atof(optarg);
+                fprintf(stderr, "Interval%10.3f\n", iflag);
                 break;
-            case 'p':
-                pflag = atoi(optarg); 
+            case 'p': // port number
+                pflag = atoi(optarg);
+                fprintf(stderr, "Port\t%10d\n", pflag);
                 break;
-            case 's':
-                sflag = atoi(optarg); 
+            case 's': // size in bytes
+                sflag = atoi(optarg);
+                fprintf(stderr, "Size\t%10d\n", sflag);
                 break;
-            case 'n':
-                nflag = true; 
+            case 'n': // no_print
+                nflag = true;
+                // fprintf(stderr, "nflag\t%10d\n");
                 break;
-            case '?': // In case we get 
+            case 'S':
+                serverflag = true;
+                break;
+            case '?':
                 if (isprint(optopt))
                     fprintf(stderr, "Unknown option `-%c'.\n", optopt);
                 else
@@ -49,15 +63,49 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("cflag = %s\n
-            iflag = %.1f\n
-            pflag = %d\n
-            sflag = %d\n
-            nflag = %d\n",
-           cflag, iflag, pflag, sflag, nflag);
-
     for (index = optind; index < argc; index++)
         printf("Non-option argument %s\n", argv[index]);
 
+    fprintf(stderr, "Running in %s mode\n", serverflag ? "server" : "client");
+    fprintf(stderr, "Port: %d\n", pflag);
+
+    /******SERVER******/
+    // Receive and Send pings
+    /*
+    if (serverflag) {
+        for (i = 0; i < atoi(cflag); i++) {
+            ReceivePing(sock); // Function prints out with stdout
+            SendPing(sock, address); 
+        }
+    }
+    */
+
+    /******CLIENT******/
+    // Send and Receive pings
+    /*
+    else {
+        for (i = 0; i < atoi(cflag); i++) {
+            SendPing(sock, address);
+            ReceivePing(sock); // Function prints out with stdout
+        }
+    }
+    */
+
+   
+
     return 0;
 }
+
+    /*
+
+    int seqNumber;
+    
+    time_t timestamp;
+    time_t currTime;
+
+    // Print ping details
+    
+    time(&timestamp);
+    time(&currentTime)
+    double roundTripTime = drifttime(currTime, timestamp)
+    */
